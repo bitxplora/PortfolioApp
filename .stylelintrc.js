302c8063e58650983a90626f1a7d3e15caaefb2b
Dayo Olutayo <olutayo@olutayo.dev>
2024-10-03 05:32:07 +0100

    Added override rules in .template-lintrc.js to allow style tag to use in template tag without triggering error. This rules is now removed from .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,12 +2,4 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  overrides: [
-    {
-      files: ['app/components/*.{gjs, gts}'],
-      rules: {
-        'no-forbidden-elements': false,
-      },
-    },
-  ],
 };
--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   extends: 'recommended',
+  overrides: [
+    {
+      files: ['app/**/*.{gjs,gts}'],
+      rules: {
+        'no-forbidden-elements': false,
+        'no-whitespace-for-layout': false,
+      },
+    },
+  ],
 };
